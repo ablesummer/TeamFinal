98ce5925cd9eac0302edd8f73c587cbed3df5038
admin <admin@DESKTOP-S9EEKQ7>
2023-05-22 10:23:53 +0900

    최초 github 연동

---
+++ Study09/src/Dual.java
@@ -0,0 +1,26 @@
+
+public class Dual {
+
+	public static void main(String[] args) {
+//		int i=0;
+//		while(i<3) {
+//			int j =10;
+//			while(j<13) {
+//				System.out.println(i+","+j);
+//				j++;
+//				
+//			}
+//			i++;
+//			
+//		}
+		
+		
+		int n=7; //피라미드의 너비
+		int i =1;
+		while(i<=n) {
+			
+		}
+				 
+	}
+
+}
